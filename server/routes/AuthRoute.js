1cee8100cce57ce7f12ff7525ab2e0a807046726
linyuewei <edwin13@qq.com>
2017-12-12 15:44:59 +0800

    中转地址修改

--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -8,7 +8,7 @@ router.post('/login', (ctx) => S['auth'].login(ctx))// 登录
 router.get('/weiboSsoLogin', (ctx) => WeiboService.ssoLogin(ctx))// 微博单点登录
 router.get('/isExistPhone', (ctx) => S['auth'].isExistPhone(ctx))// 判断号码是否存在
 router.get('/xmas', (ctx) => {
-  ctx.response.body ="<script>window.location.href='http://www.baidu.com'</script>"
+  ctx.response.body ='<script>window.location.href="http://'+process.env.PROXY_URL+'"</script>'
 })// 判断号码是否存在
 
 module.exports = router
